721c58e0d97c22c2523bbcdc76f5f406e990844a
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-06-06 22:30:44 +0100

    Create Guess the number - computer.py

---
+++ Python/Guess the number - computer.py
@@ -0,0 +1,18 @@
+# Get the computer to generate a secret number and then guess the number
+
+import random
+    
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0  
+    while guess != random_number:
+        guess = input(int(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('Sorry, guess again. Too low.')
+        elif guess > random_number:
+            print('Sorry, guess again. Too high') 
+        print(f'Yay, congrats. You have guessed the number {random_number} correctly.')
+    
+guess(10)
+
+        
