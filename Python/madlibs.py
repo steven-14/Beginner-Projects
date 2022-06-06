2570df16f481cf64e3dc4a611a28dbd77692bb95
steven-14 <104389629+steven-14@users.noreply.github.com>
2022-06-06 22:28:59 +0100

    Update madlibs.py

--- Python/madlibs.py
+++ Python/madlibs.py
@@ -1,19 +1,13 @@
 # This is my beginner project,  code along with Kylie Ying.
 # Youtube video: 12 Beginner Python Projects - Coding Course - FreeCodeCamp.org
-# Guess a number - computer
+# Madlibs string concatenation (joining words and and string numbers).
 
-import random
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
 
-def guess(x):
-    random_number = random.randint(1, x)
-    guess = 0
-    while guess != random_number:
-        #print(guess) #check
-        guess = int(input(f'Guess a number between 1 and {x}: '))
-        if guess < random_number: # this part is not running.
-            print('Sorry, guess again. Too low.')
-        elif guess > random_number:
-            print('Sorry, guess again. Too high.')
-    print(f'Yay, congrats. You have guess the number {random_number}')
-    
-guess(10)    
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
